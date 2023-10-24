74dc9fd3ff3763c1ce8c1db5b4a80060b2625456
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-10-23 21:39:25 -0400

    Update test_matchgate.py

--- tests/test_matchgate.py
+++ tests/test_matchgate.py
@@ -3,22 +3,24 @@ from msim import Matchgate
 
 
 @pytest.fixture
-def matchgate_init(*args, **kwargs) -> Matchgate:
+def matchgate_rn_init(*args, **kwargs) -> Matchgate:
     return Matchgate.random()
 
 
-def test_matchgate_m_m_dagger_constraint(matchgate_init):
-    assert matchgate_init.check_m_m_dagger_constraint()
+def test_matchgate_m_m_dagger_constraint(matchgate_rn_init):
+    assert matchgate_rn_init.check_m_m_dagger_constraint()
 
 
-def test_matchgate_m_dagger_m_constraint(matchgate_init):
-    assert matchgate_init.check_m_dagger_m_constraint()
+def test_matchgate_m_dagger_m_constraint(matchgate_rn_init):
+    assert matchgate_rn_init.check_m_dagger_m_constraint()
 
 
-def test_matchgate_det_constraint(matchgate_init):
-    assert matchgate_init.check_det_constraint()
+def test_matchgate_det_constraint(matchgate_rn_init):
+    assert matchgate_rn_init.check_det_constraint()
 
-
+def test_matchgate_constructor_with_default_theta4():
+    m = Matchgate()
+    assert m.theta4 == 0.0
 
 
 
