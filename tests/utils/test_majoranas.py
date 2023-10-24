b8d0059aadde7bae3e1a4de998edaff1180c4556
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-10-24 13:50:49 -0400

    Create test_majoranas.py

---
+++ tests/utils/test_majoranas.py
@@ -0,0 +1,13 @@
+import pytest
+from msim import utils
+
+
+
+r"""
+Test the majorana functions in order to check that they return the correct matrices.
+
+
+1. Make sure that :math:`\{c_\mu,c_\nu/} = 2\delta_{\mu\nu}I`
+
+"""
+
