658921ef9dea491a15637be84479a32b5be23ceb
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-10-24 21:15:07 -0400

    Update __init__.py

--- src/msim/__init__.py
+++ src/msim/__init__.py
@@ -1,3 +1,17 @@
+"""
+MSim is a Python library for simulating non-interacting fermionic systems using the Matchgates.
+"""
+
+__author__ = "Jérémie Gince"
+__email__ = "gincejeremie@gmail.com"
+__copyright__ = "Copyright 2023, Jérémie Gince"
+__license__ = "Apache 2.0"
+__url__ = "https://github.com/JeremieGince/FermionicSimulation"
+__version__ = "0.0.1-beta0"
+
 from .matchgate import Matchgate
 
+import warnings
 
+warnings.filterwarnings("ignore", category=Warning, module="docutils")
+warnings.filterwarnings("ignore", category=Warning, module="sphinx")
