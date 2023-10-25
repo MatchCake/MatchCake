8555704975a401dde72920c87b6bd24d72d82054
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-10-24 21:20:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-import numpy as np
-import pennylane as qml
-import matplotlib.pyplot as plt
-
-
-def circuit(params):
-    qml.QubitUnitary(U, wires=0)
-    return qml.expval(qml.PauliZ(0))
 
 
 if __name__ == '__main__':
-    simple_U = np.array([[1, 0], [0, 1]])
+    pass
 
 
 
