abf5e8e8d2094da80b0ee3e49214f8c91291056f
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-10-24 20:36:27 -0400

    Update test_matchgate.py

--- tests/test_matchgate.py
+++ tests/test_matchgate.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 from msim import Matchgate
 
@@ -19,9 +20,18 @@ def test_matchgate_det_constraint(matchgate_rn_init):
     assert matchgate_rn_init.check_det_constraint()
 
 
-def test_matchgate_constructor_with_default_theta4():
-    m = Matchgate()
-    assert m.theta4 == 0.0
+@pytest.mark.parametrize(
+    "params",
+    [
+        tuple(np.random.rand(6))
+        for _ in range(100)
+    ]
+)
+def test_matchgate_constructor_with_default_theta4(params):
+    m = Matchgate(params)
+    exp4 = np.exp(1j*np.mod(m.params.theta4, 2 * np.pi))
+    exp2 = np.exp(1j*np.mod(m.params.theta2, 2 * np.pi))
+    assert np.isclose(exp2 * exp4, 1.0+0j), f"{exp2 * exp4 = }, expected 1.0+0j"
 
 
 
