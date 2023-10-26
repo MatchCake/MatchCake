cd06205ab5d0494b34e9a9266744ee461a689871
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-10-26 12:11:47 -0400

    Update nif_device.py

--- src/msim/nif_device.py
+++ src/msim/nif_device.py
@@ -1,7 +1,11 @@
+from typing import Iterable
+
 import pennylane as qml
 from pennylane import numpy as pnp
 from .matchgate_operator import MatchgateOperator
+from .lookup_table import NonInteractingFermionicLookupTable
 from . import utils
+from pfapack import pfaffian
 
 
 class NonInteractingFermionicDevice(qml.QubitDevice):
@@ -14,8 +18,8 @@ class NonInteractingFermionicDevice(qml.QubitDevice):
     operations = {"MatchgateOperator"}
     observables = {"PauliZ"}
 
-    def __init__(self):
-        super().__init__(wires=1, shots=None)
+    def __init__(self, wires=1):
+        super().__init__(wires=wires, shots=None)
 
         # create the initial state
         self._state = pnp.array([0, 1])
@@ -24,6 +28,7 @@ class NonInteractingFermionicDevice(qml.QubitDevice):
         self._pre_rotated_state = None
         self._transition_matrix = None
         self._block_diagonal_matrix = None
+        self._lookup_table = None
 
     @property
     def state(self):
@@ -38,6 +43,17 @@ class NonInteractingFermionicDevice(qml.QubitDevice):
         if self._block_diagonal_matrix is None:
             self._block_diagonal_matrix = utils.get_block_diagonal_matrix(self.num_wires)
         return self._block_diagonal_matrix
+    
+    @property
+    def lookup_table(self):
+        if self.transition_matrix is None:
+            return None
+        if self._lookup_table is None:
+            self._lookup_table = NonInteractingFermionicLookupTable(
+                self.transition_matrix,
+                self.block_diagonal_matrix
+            )
+        return self._lookup_table
 
     @classmethod
     def capabilities(cls):
@@ -50,6 +66,9 @@ class NonInteractingFermionicDevice(qml.QubitDevice):
         return capabilities
 
     def apply(self, operations, rotations=None, **kwargs):
+        if not isinstance(operations, Iterable):
+            operations = [operations]
+        
         action_matrix = pnp.eye(2 * self.num_wires, dtype=complex)
         for op in operations:
             assert isinstance(op, MatchgateOperator), "Only MatchgateOperator is supported"
@@ -64,17 +83,20 @@ class NonInteractingFermionicDevice(qml.QubitDevice):
         # for rot in rotations or []:
         #     self._state = qml.matrix(rot) @ self._state
         assert rotations is None, "Rotations are not supported"
-        raise NotImplementedError()
 
     def analytic_probability(self, wires=None):
         if self._state is None:
             return None
-        #
-        # real = self._real(self._state)
-        # imag = self._imag(self._state)
-        # prob = self.marginal_prob(real ** 2 + imag ** 2, wires)
-        # return prob
-        raise NotImplementedError()
+        state_hamming_weight = utils.get_hamming_weight(self._state)
+        
+        if isinstance(wires, int):
+            wires = [wires]
+        assert len(wires) == 1, "Only one wire is supported for now."
+        wire = wires[0]
+        
+        obs = self.lookup_table.get_observable(wire, state_hamming_weight)
+        prob = pfaffian.pfaffian(obs)
+        return prob
 
     def reset(self):
         """Reset the device"""
