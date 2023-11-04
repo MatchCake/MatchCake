a4865369fbbd84d213920ceada88eb7461fa667d
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-11-04 01:25:13 -0400

    scratches

--- scratches/pennylane_circuits.py
+++ scratches/pennylane_circuits.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy as np
 import pennylane as qml
 from pennylane import numpy as pnp
@@ -5,44 +7,38 @@ from pennylane import numpy as pnp
 from msim import MatchgateOperator, NonInteractingFermionicDevice
 from msim import matchgate_parameter_sets as mps
 
-dev = qml.device('default.qubit', wires=2, shots=100)
-dev.operations.add(MatchgateOperator)
+
+def devices_init(*args, **kwargs) -> Tuple[NonInteractingFermionicDevice, qml.Device]:
+    nif_device = NonInteractingFermionicDevice(wires=kwargs.get("wires", 2))
+    qubit_device = qml.device('default.qubit', wires=kwargs.get("wires", 2), shots=kwargs.get("shots", 1))
+    qubit_device.operations.add(MatchgateOperator)
+    return nif_device, qubit_device
 
 
-@qml.qnode(dev)
-def circuit(params):
-    mg_params = mps.MatchgatePolarParams(*params)
-    op = MatchgateOperator(mg_params, wires=[0, 1])
+def single_matchgate_circuit(params):
+    h_params = mps.MatchgateHamiltonianCoefficientsParams.parse_from_params(params)
+    op = MatchgateOperator(h_params, wires=[0, 1])
     qml.apply(op)
-    # qml.QubitUnitary(op.matrix(), wires=[0, 1])
-    # return qml.expval(qml.Identity(wires=0))
-    return qml.probs(wires=0), qml.expval(qml.Identity(wires=0))
-
-
-mdev = NonInteractingFermionicDevice(wires=2)
-
-
-@qml.qnode(mdev)
-def mdev_circuit(params):
-    mg_params = mps.MatchgatePolarParams(*params)
-    op = MatchgateOperator(mg_params, wires=[0, 1])
-    qml.apply(op)
-    # return qml.expval(qml.Identity(wires=0))
-    return qml.probs(wires=0), qml.expval(qml.Identity(wires=0))
+    return qml.probs(wires=0)
 
 
 if __name__ == '__main__':
-    
-    print(f"{dev.state = }")
-    print(f"{mdev.state = }")
-    
-    # input_params = np.array([1.0, 1.0, 0.0, 0.0, 0.0, 0.0])
+    nif_device, qubit_device = devices_init()
+    print(f"{qubit_device.state = }")
+    print(f"{nif_device.state = }")
+
     input_params = np.random.rand(6)
-    # print(qml.draw(circuit)(input_params))
-    print(f"{circuit(input_params) = }")
-    
-    # print(qml.draw(mdev_circuit)(input_params))
-    print(f"{mdev_circuit(input_params) = }")
+    # input_params = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+
+    nif_qnode = qml.QNode(single_matchgate_circuit, nif_device)
+    qubit_qnode = qml.QNode(single_matchgate_circuit, qubit_device)
+
+    qubit_probs = qubit_qnode(input_params)
+    print(f"{qubit_probs = }")
+    nif_probs = nif_qnode(input_params)
+    print(f"{nif_probs = }")
+    check = np.allclose(nif_probs, qubit_probs)
+    print(f"{check = }")
 
 
 
