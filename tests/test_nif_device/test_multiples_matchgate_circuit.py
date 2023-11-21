779084fafce0750ee2afed3373b9bd68297db90e
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-11-21 13:49:19 -0500

    Update test_multiples_matchgate_circuit.py

--- tests/test_nif_device/test_multiples_matchgate_circuit.py
+++ tests/test_nif_device/test_multiples_matchgate_circuit.py
@@ -4,7 +4,7 @@ import numpy as np
 import pennylane as qml
 import pytest
 
-from msim import MatchgateOperator
+from msim import MatchgateOperator, utils
 from msim import matchgate_parameter_sets as mps
 from . import devices_init
 from ..configs import N_RANDOM_TESTS_PER_CASE
@@ -12,58 +12,69 @@ from ..configs import N_RANDOM_TESTS_PER_CASE
 np.random.seed(42)
 
 
-def multiples_matchgate_circuit(params_list, all_wires=None):
+def multiples_matchgate_circuit(params_list, initial_state=None, **kwargs):
+    all_wires = kwargs.get("all_wires", None)
     if all_wires is None:
         all_wires = [0, 1]
     all_wires = np.sort(np.asarray(all_wires))
+    if initial_state is None:
+        initial_state = np.zeros(len(all_wires), dtype=int)
+    qml.BasisState(initial_state, wires=all_wires)
     for params in params_list:
         mg_params = mps.MatchgatePolarParams.parse_from_any(params)
         wire0 = np.random.choice(all_wires[:-1], size=1).item()
         wire1 = wire0 + 1
         MatchgateOperator(mg_params, wires=[wire0, wire1])
-    return qml.probs(wires=0)
+    out_op = kwargs.get("out_op", "state")
+    if out_op == "state":
+        return qml.state()
+    elif out_op == "probs":
+        return qml.probs(wires=kwargs.get("out_wires", None))
+    else:
+        raise ValueError(f"Unknown out_op: {out_op}.")
 
 
 @pytest.mark.parametrize(
-    "params_list,n_wires",
+    "params_list,n_wires,prob_wires",
     [
-        ([mps.MatchgatePolarParams(r0=1, r1=1).to_numpy() for _ in range(num_gates)], num_wires)
-        for num_gates in range(1, 3)
-        for num_wires in range(2, 6)
+        ([mps.MatchgatePolarParams.random(), mps.MatchgatePolarParams.random()], 4, 0)
     ]
-    +
-    [
-        ([mps.MatchgatePolarParams.random().to_numpy() for _ in range(num_gates)], num_wires)
-        for _ in range(N_RANDOM_TESTS_PER_CASE)
-        for num_gates in range(1, 3)
-        for num_wires in range(2, 6)
-    ]
-    +
-    [
-        ([mps.MatchgatePolarParams(
-            r0=np.random.uniform(0.00, 0.01),
-            r1=np.random.uniform(0.00, 0.01),
-            theta0=np.random.uniform(-np.pi, np.pi) / 1,
-            theta1=np.random.uniform(-np.pi, np.pi) / 1,
-            theta2=np.random.uniform(-np.pi, np.pi) / 1,
-            theta3=np.random.uniform(-np.pi, np.pi) / 1,
-        ).to_numpy() for _ in range(num_gates)], num_wires)
-        for _ in range(N_RANDOM_TESTS_PER_CASE)
-        for num_gates in range(1, 3)
-        for num_wires in range(2, 6)
-    ]
+    # [
+    #     ([mps.MatchgatePolarParams(r0=1, r1=1).to_numpy() for _ in range(num_gates)], num_wires, 0)
+    #     for num_gates in range(1, 4)
+    #     for num_wires in range(2, 6)
+    # ]
+    # +
+    # [
+    #     ([mps.MatchgatePolarParams.random().to_numpy() for _ in range(num_gates)], num_wires, 0)
+    #     for _ in range(N_RANDOM_TESTS_PER_CASE)
+    #     for num_gates in range(1, 4)
+    #     for num_wires in range(2, 6)
+    # ]
 )
-def test_multiples_matchgate_probs_with_qbit_device(params_list, n_wires):
+def test_multiples_matchgate_probs_with_qbit_device(params_list, n_wires, prob_wires):
     nif_device, qubit_device = devices_init(wires=n_wires)
 
-    multiples_matchgate_circuit_func = partial(multiples_matchgate_circuit, all_wires=list(range(n_wires)))
-    nif_qnode = qml.QNode(multiples_matchgate_circuit_func, nif_device)
-    qubit_qnode = qml.QNode(multiples_matchgate_circuit_func, qubit_device)
-
-    nif_probs = nif_qnode(params_list)
-    qubit_probs = qubit_qnode(params_list)
-    same_argmax = np.argmax(nif_probs) == np.argmax(qubit_probs)
-    assert same_argmax, (f"The argmax is not the correct one. "
-                         f"Got {np.argmax(nif_probs)} instead of {np.argmax(qubit_probs)}")
-    check = np.allclose(nif_probs, qubit_probs, rtol=1.e-1, atol=1.e-1)
+    nif_qnode = qml.QNode(multiples_matchgate_circuit, nif_device)
+    qubit_qnode = qml.QNode(multiples_matchgate_circuit, qubit_device)
+    
+    initial_binary_state = np.zeros(n_wires, dtype=int)
+    qubit_state = qubit_qnode(
+        params_list,
+        initial_binary_state,
+        all_wires=qubit_device.wires,
+        in_param_type=mps.MatchgatePolarParams,
+        out_op="state",
+    )
+    qubit_probs = utils.get_probabilities_from_state(qubit_state, wires=prob_wires)
+    nif_probs = nif_qnode(
+        params_list,
+        initial_binary_state,
+        all_wires=nif_device.wires,
+        in_param_type=mps.MatchgatePolarParams,
+        out_op="probs",
+        out_wires=prob_wires,
+    )
+    
+    check = np.allclose(nif_probs, qubit_probs, rtol=1.e-3, atol=1.e-3)
     assert check, f"The probs are not the correct one. Got {nif_probs} instead of {qubit_probs}"
