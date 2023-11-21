607bc6efbbf5557bbe428cc885c6c939f1360381
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-11-20 22:09:20 -0500

    Update __init__.py

--- src/msim/matchgate_parameter_sets/__init__.py
+++ src/msim/matchgate_parameter_sets/__init__.py
@@ -5,4 +5,13 @@ from .matchgate_standard_hamiltonian_params import MatchgateStandardHamiltonianP
 from .matchgate_standard_params import MatchgateStandardParams
 from .matchgate_polar_params import MatchgatePolarParams
 from . import transfer_functions
+from .. import utils
 
+Identity = MatchgateStandardParams(
+    a=1, b=0, c=0, d=1,
+    w=1, x=0, y=0, z=1,
+)
+fSWAP = MatchgateStandardParams(
+    a=utils.PAULI_Z[0, 0], b=utils.PAULI_Z[0, 1], c=utils.PAULI_Z[1, 0], d=utils.PAULI_Z[1, 1],
+    w=utils.PAULI_X[0, 0], x=utils.PAULI_X[0, 1], y=utils.PAULI_X[1, 0], z=utils.PAULI_X[1, 1]
+)
