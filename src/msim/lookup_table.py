62dafa39fe9cac82757afbc7b7b2cc8389dbec9c
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-11-23 15:01:31 -0500

    Update lookup_table.py

--- src/msim/lookup_table.py
+++ src/msim/lookup_table.py
@@ -187,3 +187,37 @@ class NonInteractingFermionicLookupTable:
             obs[i, j] = self[row, col][i_k, j_k]
         obs = obs - obs.T
         return obs
+    
+    def compute_observable_of_target_state(
+            self,
+            system_state: np.ndarray,
+            target_binary_state: Optional[np.ndarray] = None,
+            indexes_of_target_state: Optional[np.ndarray] = None,
+    ) -> np.ndarray:
+        if target_binary_state is None and indexes_of_target_state is None:
+            target_binary_state = np.array([1, ])
+            indexes_of_target_state = np.array([0, ])
+        elif target_binary_state is not None and indexes_of_target_state is None:
+            indexes_of_target_state = np.arange(len(target_binary_state), dtype=int)
+        elif target_binary_state is None and indexes_of_target_state is not None:
+            target_binary_state = np.ones(len(indexes_of_target_state), dtype=int)
+            
+        ket_majorana_indexes = utils.decompose_state_into_majorana_indexes(system_state)
+        bra_majorana_indexes = list(reversed(ket_majorana_indexes))
+
+        unmeasured_cls_indexes = [2 for _ in range(len(ket_majorana_indexes))]
+        measure_cls_indexes = np.array([[1, 0] for _ in range(k + 1)]).flatten().tolist()
+        lt_indexes = unmeasured_cls_indexes + measure_cls_indexes + unmeasured_cls_indexes
+
+        # measure_indexes = np.array([[i, i] for i in range(k+1)]).flatten().tolist()
+        measure_indexes = [k, k]
+        majorana_indexes = list(bra_majorana_indexes) + measure_indexes + list(ket_majorana_indexes)
+
+        obs_size = len(majorana_indexes)
+        obs = np.zeros((obs_size, obs_size), dtype=complex)
+        for (i, j) in zip(*np.triu_indices(obs_size, k=1)):
+            i_k, j_k = majorana_indexes[i], majorana_indexes[j]
+            row, col = lt_indexes[i], lt_indexes[j]
+            obs[i, j] = self[row, col][i_k, j_k]
+        obs = obs - obs.T
+        return obs
