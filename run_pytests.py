002f8a300bfce8ee94abf84b15bf6f3b0d189878
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-11-26 16:23:24 -0500

    Update run_pytests.py

--- run_pytests.py
+++ run_pytests.py
@@ -4,6 +4,7 @@ import os
 import pytest
 import pythonbasictools as pbt
 from pytest_jsonreport.plugin import JSONReport
+from pytest_cov.plugin import CovPlugin
 
 from tests import configs
 
@@ -16,11 +17,12 @@ if __name__ == '__main__':
         }
     )
     configs.N_RANDOM_TESTS_PER_CASE = sys_args_dict["N_RANDOM_TESTS_PER_CASE"]
-    plugin = JSONReport()
-    pytest.main([sys_args_dict[1]], plugins=[plugin])
+    json_plugin = JSONReport()
+    pytest.main([sys_args_dict[1]], plugins=[json_plugin])
     json_path = os.path.join(os.getcwd(), "tests", "tmp", f"tests_report_rn{configs.N_RANDOM_TESTS_PER_CASE}.json")
-    if bool(sys_args_dict["save_report"]):
-        plugin.save_report(json_path)
+    save_report = 't' in str(sys_args_dict["save_report"]).lower()
+    if save_report:
+        json_plugin.save_report(json_path)
         json_data = json.load(open(json_path))
         with open(json_path, "w") as f:
             json.dump(json_data, f, indent=4)
