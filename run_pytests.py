941785f93caf0d6f593745d23ea9931dd26142de
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-11-25 23:34:42 -0500

    Update run_pytests.py

--- run_pytests.py
+++ run_pytests.py
@@ -12,13 +12,15 @@ if __name__ == '__main__':
         {
             1                        : os.path.join(os.getcwd(), "tests"),
             "N_RANDOM_TESTS_PER_CASE": configs.N_RANDOM_TESTS_PER_CASE,
+            "save_report"            : "True",
         }
     )
     configs.N_RANDOM_TESTS_PER_CASE = sys_args_dict["N_RANDOM_TESTS_PER_CASE"]
     plugin = JSONReport()
     pytest.main([sys_args_dict[1]], plugins=[plugin])
-    json_path = os.path.join(os.getcwd(), "tests", "tmp", "tests_report.json")
-    plugin.save_report(json_path)
-    json_data = json.load(open(json_path))
-    with open(json_path, "w") as f:
-        json.dump(json_data, f, indent=4)
+    json_path = os.path.join(os.getcwd(), "tests", "tmp", f"tests_report_rn{configs.N_RANDOM_TESTS_PER_CASE}.json")
+    if bool(sys_args_dict["save_report"]):
+        plugin.save_report(json_path)
+        json_data = json.load(open(json_path))
+        with open(json_path, "w") as f:
+            json.dump(json_data, f, indent=4)
