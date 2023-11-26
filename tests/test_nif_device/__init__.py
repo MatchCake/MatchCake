907df4728c3c8452b1676e8012b453f183a976da
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-11-26 16:23:29 -0500

    Update __init__.py

--- tests/test_nif_device/__init__.py
+++ tests/test_nif_device/__init__.py
@@ -1,22 +1,26 @@
 from typing import Tuple
 
-import numpy as np
-import pytest
 import pennylane as qml
-from msim import MatchgateOperator, NonInteractingFermionicDevice, Matchgate
-from msim import matchgate_parameter_sets as mps
-from msim import utils
-from functools import partial
 
-from msim.utils import PAULI_Z, PAULI_X
+from msim import MatchgateOperator, NonInteractingFermionicDevice
+from msim import utils
 from .test_single_matchgate_circuit import single_matchgate_circuit
+
+_majorana_getters = {}
 
 
 def devices_init(*args, **kwargs) -> Tuple[NonInteractingFermionicDevice, qml.Device]:
-    nif_device = NonInteractingFermionicDevice(
-        wires=kwargs.get("wires", 2), prob_strategy=kwargs.pop("prob_strategy", "lookup_table")
-    )
-    qubit_device = qml.device('default.qubit', wires=kwargs.get("wires", 2), shots=kwargs.get("shots", None))
+    wires = kwargs.pop("wires", 2)
+    qubit_device = qml.device('default.qubit', wires=wires, shots=kwargs.get("shots", None))
     qubit_device.operations.add(MatchgateOperator)
+    majorana_getter = _majorana_getters.get(
+        qubit_device.num_wires,
+        utils.majorana.MajoranaGetter(qubit_device.num_wires, maxsize=kwargs.pop("maxsize", 1024)),
+    )
+    _majorana_getters[qubit_device.num_wires] = majorana_getter
+    nif_device = NonInteractingFermionicDevice(
+        wires=wires,
+        prob_strategy=kwargs.pop("prob_strategy", "lookup_table"),
+        majorana_getter=majorana_getter,
+    )
     return nif_device, qubit_device
-
