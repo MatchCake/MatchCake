88ce3924a7b1e506b8ad248d76858cce08181463
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-12-09 15:36:47 -0500

    Update angle_embedding.py

--- src/msim/operations/angle_embedding.py
+++ src/msim/operations/angle_embedding.py
@@ -2,6 +2,7 @@ import pennylane as qml
 from pennylane.wires import Wires
 from pennylane.operation import Operation, AnyWires
 from .m_rot import MRot
+import numpy as np
 
 
 class MAngleEmbedding(Operation):
@@ -13,17 +14,23 @@ class MAngleEmbedding(Operation):
         batched = qml.math.ndim(params) > 1
         params = qml.math.T(params) if batched else params
         wires = Wires(wires)
-        if len(wires) % 2 != 0:
-            raise ValueError(
-                f"MAngleEmbedding requires an even number of wires; got {len(wires)}."
-            )
-        wires_tuple = zip(wires[::2], wires[1::2])
+        w0 = np.asarray(wires) % max(wires)
+        wires_tuple = list(zip(w0, w0 + 1))
         return [MRot(params[i], wires=[w0, w1]) for i, (w0, w1) in enumerate(wires_tuple)]
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.data}, wires={self.wires.tolist()})"
 
     def __init__(self, features, wires, id=None):
+        r"""
+        Construct a new Matchgate AngleEmbedding operation.
+
+        :Note: It is better to have one more wire than the number of features because every gate acts on two wires.
+
+        :param features: The features to embed.
+        :param wires: The wires to embed the features on.
+        :param id: The id of the operation.
+        """
         shape = qml.math.shape(features)[-1:]
         n_features = shape[0]
         if n_features > len(wires):
