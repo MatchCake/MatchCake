43d3a0325278632ea379e370f3e97c809c20c773
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2024-01-13 08:49:18 -0500

    Update main.py

--- benchmark/classification/main.py
+++ benchmark/classification/main.py
@@ -18,6 +18,7 @@ def parse_args():
     parser.add_argument("--trial", type=str, default="cuda_det")
     parser.add_argument("--show_n_pts", type=int, default=512)
     parser.add_argument("--debug_data_size", type=int, default=None)
+    parser.add_argument("--overwrite", type=bool, default=False)
     return parser.parse_args()
 
 
@@ -48,29 +49,29 @@ def main():
         n_kfold_splits=args.n_kfold_splits,
         kernel_kwargs=dict(nb_workers=0, batch_size=args.batch_size, use_cuda=use_cuda),
         throw_errors=args.throw_errors,
+        use_gram_matrices=True,
     )
     save_path = os.path.join(
         args.save_dir, args.trial, f"{kwargs['dataset_name']}", f"cls.pkl"
     )
-    pipline = ClassificationPipeline.from_pickle_or_new(
-        **kwargs,
-        save_path=save_path,
-        use_gram_matrices=True,
-    )
+    if args.overwrite:
+        pipeline = ClassificationPipeline(**kwargs)
+    else:
+        pipeline = ClassificationPipeline.from_pickle_or_new(save_path=save_path, **kwargs)
     figures_folder = os.path.join(os.path.dirname(save_path), "figures")
-    pipline.load_dataset()
-    pipline.preprocess_data()
-    pipline.print_summary()
-    pipline.run(results_table_path=os.path.join(figures_folder, "results_table.csv"))
-    results = pipline.get_results_table(show=True, filepath=os.path.join(figures_folder, "results_table.csv"))
-    mean_results = pipline.get_results_table(
+    pipeline.load_dataset()
+    pipeline.preprocess_data()
+    pipeline.print_summary()
+    pipeline.run(results_table_path=os.path.join(figures_folder, "results_table.csv"))
+    results = pipeline.get_results_table(show=True, filepath=os.path.join(figures_folder, "results_table.csv"))
+    mean_results = pipeline.get_results_table(
         show=True,
         mean=True,
         filepath=os.path.join(figures_folder, "results_table_mean.csv")
     )
-    pipline.draw_mpl_kernels(show=False, filepath=os.path.join(figures_folder, "kernels.pdf"), draw_mth="single")
+    pipeline.draw_mpl_kernels(show=False, filepath=os.path.join(figures_folder, "kernels.pdf"), draw_mth="single")
     plt.close("all")
-    pipline.show(n_pts=args.show_n_pts, show=True, filepath=os.path.join(figures_folder, "decision_boundaries.pdf"))
+    pipeline.show(n_pts=args.show_n_pts, show=True, filepath=os.path.join(figures_folder, "decision_boundaries.pdf"))
     plt.close("all")
 
 
