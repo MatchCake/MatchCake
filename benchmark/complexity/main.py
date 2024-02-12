ea187ee9d134ea5408c46ae11fd780c9dc9e608d
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2024-02-11 23:17:54 -0500

    Update main.py

--- benchmark/complexity/main.py
+++ benchmark/complexity/main.py
@@ -21,14 +21,14 @@ def filter_odd_wires_number(wires) -> np.ndarray:
 def main(kwargs):
     matplotlib.rcParams.update(MPL_RC_DEFAULT_PARAMS)
     max_n_wires = [n for n in BenchmarkPipeline.max_wires_methods.values() if np.isfinite(n)]
-    n_wires_range = (2, 128, 10)
+    n_wires_range = (2, 1024, 3)
     n_wires = np.linspace(n_wires_range[0], n_wires_range[1], num=n_wires_range[2], endpoint=True, dtype=int).tolist()
     n_wires = list(sorted(set(n_wires + max_n_wires)))
     n_wires = filter_odd_wires_number(n_wires)
     # n_wires = list(sorted(set([2, 128, 1024, ] + max_n_wires)))
     n_wires = kwargs.get("n_wires", n_wires)
     n_wires_str = n_wires if isinstance(n_wires, str) else "-".join(map(str, n_wires_range))
-    n_gates = int(kwargs.get("n_gates", 10 * np.max(n_wires)))
+    n_gates = int(kwargs.get("n_gates", 1 * np.max(n_wires)))
     # n_gates = 1
     # n_gates = "linear"
     # n_wires = np.arange(2, 1024, 100)
@@ -42,14 +42,14 @@ def main(kwargs):
         n_wires=n_wires,
         n_gates=n_gates,
         # methods=kwargs.get("methods", ["nif.lookup_table", "default.qubit", "nif.explicit_sum"]),
-        # methods=kwargs.get("methods", ["nif.lookup_table", "default.qubit", "nif.cuda"]),
-        methods=kwargs.get("methods", ["nif.lookup_table", "default.qubit"]),
+        methods=kwargs.get("methods", ["nif.lookup_table", "default.qubit", "nif.cuda"]),
+        # methods=kwargs.get("methods", ["nif.lookup_table", "default.qubit"]),
         save_path=f"{folder}/objects",
         # circuit=angle_embedding_circuit,
         # n_params_per_gate=kwargs.get("n_params_per_gate", "wires"),
     )
     benchmark_pipeline.run(
-        # nb_workers=-2,
+        nb_workers=-2,
         overwrite=kwargs.get("overwrite", False),
     )
     benchmark_pipeline.show(
@@ -57,7 +57,12 @@ def main(kwargs):
         xaxis="n_wires",
         yaxis="time",
         std_coeff=std_coeff,
-        methods_names={"nif.lookup_table": "NIF", "default.qubit": "Pennylane", "nif.explicit_sum": "NIF.es"},
+        methods_names={
+            "nif.lookup_table": "NIF",
+            "default.qubit": "Pennylane",
+            # "nif.explicit_sum": "NIF.es",
+            "nif.cuda": "NIF.cuda",
+        },
         # pt_indexes=[0, 1, 2],
         n_ticks=kwargs.get("n_ticks", 3),
         # pt_indexes_ticks=[0, -3, -2, -1],
@@ -74,6 +79,7 @@ def main(kwargs):
             "nif.lookup_table": "NIF",
             "default.qubit": "Pennylane",
             # "nif.explicit_sum": "NIF.es",
+            "nif.cuda": "NIF.cuda",
         },
         # pt_indexes=[0, 1, 2],
         n_ticks=kwargs.get("n_ticks", 3),
