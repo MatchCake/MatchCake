ef431cf266bab63fc676f18f7939c7760317494c
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2024-02-27 17:46:09 -0500

    Update dataset_complexity.py

--- benchmark/classification/dataset_complexity.py
+++ benchmark/classification/dataset_complexity.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 import psutil
 import pennylane as qml
+from numpy.polynomial import Polynomial
 from scipy import stats
 from sklearn import datasets
 from sklearn import svm
@@ -18,7 +19,7 @@ from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.utils.validation import check_is_fitted
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score, r2_score
 import functools
 import umap
 from tqdm import tqdm
@@ -59,7 +60,7 @@ from matchcake.ml import ClassificationVisualizer
 from matchcake.ml.ml_kernel import MLKernel, FixedSizeSVC
 import warnings
 from classification_pipeline import ClassificationPipeline
-from utils import MPL_RC_DEFAULT_PARAMS
+from utils import MPL_RC_BIG_FONT_PARAMS, mStyles, find_complexity
 
 
 class DatasetComplexityPipeline:
@@ -227,6 +228,9 @@ class DatasetComplexityPipeline:
         kernel_to_color = kwargs.get("kernel_to_color", None)
         if kernel_to_color is None:
             kernel_to_color = {k: colors[i] for i, k in enumerate(kernels)}
+        kernel_to_marker = kwargs.get("kernel_to_marker", None)
+        if kernel_to_marker is None:
+            kernel_to_marker = {k: mStyles[i] for i, k in enumerate(kernels)}
         kernel_to_linestyle = kwargs.get("kernel_to_linestyle", None)
         base_linestyle = kwargs.get("linestyle", "-")
         if kernel_to_linestyle is None:
@@ -235,6 +239,8 @@ class DatasetComplexityPipeline:
         if kernel_to_lbl is None:
             kernel_to_lbl = {k: k for k in kernels}
         y_min, y_max = np.inf, -np.inf
+        data_dict = {}
+        kernel_to_polyfit = kwargs.get("polyfit", {})
         for i, (kernel_name, kernel_df) in enumerate(df.groupby(ClassificationPipeline.KERNEL_KEY)):
             if kernel_name not in kernels:
                 continue
@@ -242,11 +248,27 @@ class DatasetComplexityPipeline:
             y_series = kernel_df.groupby(x_axis_key)[y_axis_key]
             y_mean = y_scale_factor * y_series.mean().values
             y_std = y_scale_factor * y_series.std().values
-            pre_lbl, post_lbl = kwargs.get("pre_lbl", ""), kwargs.get("post_lbl", "")
-            lbl = f"{pre_lbl}{kernel_to_lbl.get(kernel_name, kernel_name)}{post_lbl}"
+
             k_color = kernel_to_color.get(kernel_name, colors[i])
+            k_marker = kernel_to_marker.get(kernel_name, mStyles[i])
             k_linestyle = kernel_to_linestyle.get(kernel_name, base_linestyle)
-            ax.plot(x_sorted_unique, y_mean, label=lbl, color=k_color, linestyle=k_linestyle)
+
+            if kernel_to_polyfit.get(kernel_name, False):
+                *_, comp_out = self.add_complexity(
+                    fig=fig, ax=ax,
+                    x=x_sorted_unique,
+                    all_x=np.sort(df[x_axis_key].unique()),
+                    y=y_mean,
+                    color=k_color,
+                    alpha=0.5,
+                    linestyle=":",
+                )
+                comp_lbl = comp_out["label"]
+            else:
+                comp_out, comp_lbl = None, ""
+            pre_lbl, post_lbl = kwargs.get("pre_lbl", ""), kwargs.get("post_lbl", "")
+            lbl = f"{pre_lbl}{kernel_to_lbl.get(kernel_name, kernel_name)}{post_lbl}{comp_lbl}"
+            ax.plot(x_sorted_unique, y_mean, label=lbl, color=k_color, marker=k_marker, linestyle=k_linestyle)
 
             conf_int_a, conf_int_b = stats.norm.interval(
                 confidence_interval, loc=y_mean,
@@ -254,23 +276,36 @@ class DatasetComplexityPipeline:
             )
             ax.fill_between(x_sorted_unique, conf_int_a, conf_int_b, alpha=0.2, color=k_color)
             y_min, y_max = min(y_min, np.nanmin(conf_int_a)), max(y_max, np.nanmax(conf_int_b))
+            data_dict[kernel_name] = {
+                "x": x_sorted_unique,
+                "y": y_mean,
+                "y_std": y_std,
+                "y_min": y_min,
+                "y_max": y_max,
+                "conf_int_a": conf_int_a,
+                "conf_int_b": conf_int_b,
+                "complexity_out": comp_out,
+                "complexity_lbl": comp_lbl,
+            }
+
         ax.set_xlabel(kwargs.get("x_axis_label", x_axis_key))
         ax.set_ylabel(kwargs.get("y_axis_label", y_axis_key))
         # ax.set_xlim(x_min, x_max)
         # ax.set_ylim(y_min, y_max)
+        data_dict["y_min"], data_dict["y_max"] = y_min, y_max
         if kwargs.get("legend", True):
             ax.legend()
         if kwargs.get("show", False):
             plt.show()
-        if kwargs.get("return_y_min_max", False):
-            return fig, ax, y_min, y_max
+        if kwargs.get("return_data_dict", False):
+            return fig, ax, data_dict
         return fig, ax
 
     def plot_formatted_complexity_results(self, **kwargs):
         if kwargs.get("use_default_rc_params", True):
-            plt.rcParams.update(MPL_RC_DEFAULT_PARAMS)
+            plt.rcParams.update(MPL_RC_BIG_FONT_PARAMS)
         x_keys = {
-            "Kernel size [-]": "kernel_size",
+            "N [-]": "kernel_size",
             # "n_features": "Number of features [-]",
         }
         accuracies_lbl = "Accuracies [%]"
@@ -292,7 +327,13 @@ class DatasetComplexityPipeline:
         kernels = df[ClassificationPipeline.KERNEL_KEY].unique()
         gpu_methods = [m for m in kernels if "cuda" in m]
         cpu_methods = [m for m in kernels if m not in gpu_methods]
+        poly_methods = [m for m in cpu_methods if m != "PQC"]
+        exp_methods = [m for m in cpu_methods if m == "PQC"]
         set_methods = cpu_methods + [m for m in gpu_methods if m.replace("-cuda", "-cpu") not in cpu_methods]
+        y_lbl_to_polyfit = {
+            ClassificationPipeline.FIT_TIME_KEY: {**{m: True for m in cpu_methods}, **{m: False for m in gpu_methods}},
+            accuracies_lbl: {m: False for m in kernels},
+        }
         y_lbl_to_kernels_list = {
             ClassificationPipeline.FIT_TIME_KEY: kernels,
             accuracies_lbl: set_methods
@@ -311,45 +352,69 @@ class DatasetComplexityPipeline:
         sorted_lbls = sorted(set(kernel_to_lbl.values()))
         colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
         kernel_lbl_to_color = {lbl: colors[i] for i, lbl in enumerate(sorted_lbls)}
+        kernel_lbl_to_marker = {lbl: mStyles[i] for i, lbl in enumerate(sorted_lbls)}
         kernel_to_color = {k: kernel_lbl_to_color[lbl] for k, lbl in kernel_to_lbl.items()}
+        kernel_to_marker = {k: kernel_lbl_to_marker[lbl] for k, lbl in kernel_to_lbl.items()}
+
         for x_lbl, x_key in x_keys.items():
             n_rows = int(np.sqrt(len(y_lbl_to_keys)))
             n_cols = int(np.ceil(len(y_lbl_to_keys) / n_rows))
             fig, axes = plt.subplots(n_rows, n_cols, figsize=(10 * n_cols, 10 * n_rows))
             axes = np.ravel(np.asarray([axes]))
+            data_dict = {}
             for i, (y_lbl, y_key) in enumerate(y_lbl_to_keys.items()):
                 if not isinstance(y_key, list):
                     y_key = [y_key]
                 y_min, y_max = np.inf, -np.inf
                 for j, y_k in enumerate(y_key):
-                    *_, j_y_min, j_y_max = self.plot_results(
+                    *_, j_data_dict = self.plot_results(
                         x_axis_key=x_key, x_axis_label="",
                         y_axis_key=y_k, y_axis_label=y_lbl,
                         kernels=y_lbl_to_kernels_list[y_lbl],
                         pre_lbl=y_lbl_to_pre_post_lbl.get(y_lbl, [""] * (j + 1))[j],
                         kernel_to_lbl=kernel_to_lbl,
                         kernel_to_color=kernel_to_color,
+                        kernel_to_marker=kernel_to_marker,
                         y_scale_factor=y_lbl_to_scale_factor.get(y_lbl, 1),
                         linestyle=linestyles[j],
                         kernel_to_linestyle=y_lbl_to_linestyle_list[y_lbl][j],
+                        polyfit=y_lbl_to_polyfit.get(y_lbl, {}),
                         fig=fig, ax=axes[i],
                         legend=False,
                         show=False,
-                        return_y_min_max=True,
+                        return_data_dict=True,
                     )
+                    data_dict[y_k] = j_data_dict
+                    j_y_min, j_y_max = j_data_dict["y_min"], j_data_dict["y_max"]
                     y_min, y_max = min(y_min, j_y_min), max(y_max, j_y_max)
+
                 x_min, x_max = np.nanmin(df[x_key].values), np.nanmax(df[x_key].values)
                 axes[i].set_xlim(x_min, x_max)
                 axes[i].set_ylim(y_min, y_max)
+
+            # poly_y = np.mean([data_dict[ClassificationPipeline.FIT_TIME_KEY][m]["y"] for m in poly_methods], axis=0)
+            # self.add_polyfit(
+            #     fig=fig, ax=axes[0],
+            #     x=data_dict[ClassificationPipeline.FIT_TIME_KEY][poly_methods[0]]["x"],
+            #     y=poly_y,
+            #     color="gray",
+            #     linestyle="-.",
+            # )
             fig.supxlabel(x_lbl)
             # create a legend with custom patches using the kernel_to_linestyle and kernel_to_color
-            patches = [
-                plt.Line2D([0], [0], color=c, label=lbl)
+            lbl_to_kernel = {v: k for k, v in kernel_to_lbl.items()}
+            lbl_to_color_marker = {
+                f"{lbl}{data_dict[ClassificationPipeline.FIT_TIME_KEY][lbl_to_kernel[lbl]]['complexity_lbl']}":
+                    (c, kernel_to_marker[lbl_to_kernel[lbl]])
                 for lbl, c in kernel_lbl_to_color.items()
+            }
+            patches = [
+                plt.Line2D([0], [0], color=c, label=lbl, marker=mk)
+                for lbl, (c, mk) in lbl_to_color_marker.items()
             ]
             patches += [
                 plt.Line2D([0], [0], color="black", label=lbl, linestyle=ls)
-                for lbl, ls in [("GPU", gpu_linestyle), ("CPU", cpu_linestyle)]
+                for lbl, ls in [("GPU", gpu_linestyle), ("CPU", cpu_linestyle), ("Polyfit", ":")]
             ]
             axes[0].legend(handles=patches, loc='upper left')
             patches = [
@@ -367,6 +432,67 @@ class DatasetComplexityPipeline:
                 plt.show()
             plt.close("all")
         return
+
+    def add_polyfit(self, **kwargs):
+        fig: plt.Figure = kwargs.get("fig", None)
+        ax: plt.Axes = kwargs.get("ax", None)
+        if fig is None or ax is None:
+            raise ValueError("fig and ax must be provided.")
+        x = kwargs.get("x")
+        y = kwargs.get("y")
+        nan_mask = np.isnan(x) | np.isnan(y)
+        x = x[~nan_mask]
+        y = y[~nan_mask]
+
+        all_x = kwargs.get("all_x", x)
+        color = kwargs.get("color", "gray")
+        alpha = kwargs.get("alpha", 0.5)
+        linestyle = kwargs.get("linestyle", ":")
+
+        poly_list = [Polynomial.fit(x, y, deg) for deg in x.astype(int)]
+        r2_list = [r2_score(y, poly(x)) for poly in poly_list]
+        complexity = x.astype(int)[np.argmax(r2_list)]
+
+        poly = Polynomial.fit(x, y, complexity)
+        complexity_coeff = poly.coef[0]
+        x_fit = np.linspace(all_x[0], all_x[-1], 1_000)
+        y_fit = poly(x_fit)
+        r2 = r2_score(y, poly(x))
+        output = {
+            "poly": poly,
+            "r2": r2,
+            "poly_list": poly_list,
+            "r2_list": r2_list,
+            "complexity": complexity,
+            "complexity_coeff": complexity_coeff,
+        }
+
+        ax.plot(x_fit, y_fit, color=color, linestyle=linestyle, alpha=alpha)
+        return fig, ax, output
+
+    def add_complexity(self, x, y, **kwargs):
+        fig: plt.Figure = kwargs.get("fig", None)
+        ax: plt.Axes = kwargs.get("ax", None)
+        if fig is None or ax is None:
+            raise ValueError("fig and ax must be provided.")
+        nan_mask = np.isnan(x) | np.isnan(y)
+        x = x[~nan_mask]
+        y = y[~nan_mask]
+
+        all_x = kwargs.get("all_x", x)
+        color = kwargs.get("color", "gray")
+        alpha = kwargs.get("alpha", 0.5)
+        linestyle = kwargs.get("linestyle", ":")
+
+        complexity_out = find_complexity(x, y, x_lbl="N")
+        complexity_func = complexity_out["func"]
+        complexity_lbl = complexity_out["label"]
+        x_fit = np.linspace(all_x[0], all_x[-1], 1_000)
+        y_fit = complexity_func(x_fit, *complexity_out["popt"])
+        r2 = complexity_out["r_squared"]
+        ax.plot(x_fit, y_fit, color=color, linestyle=linestyle, alpha=alpha)
+        return fig, ax, complexity_out
+
 
 
 def parse_args():
@@ -420,7 +546,7 @@ def parse_args():
         help="Whether to throw errors or not."
     )
     parser.add_argument("--n_samples", type=int, default=None)
-    parser.add_argument("--run_pipelines", action=argparse.BooleanOptionalAction, default=True)
+    parser.add_argument("--run_pipelines", action=argparse.BooleanOptionalAction, default=False)
     return parser.parse_args()
 
 
