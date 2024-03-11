22aea3704a988f48f38fa60149b8c4cc0e376cb9
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2024-03-11 09:43:21 -0400

    Update run_pytests.py

--- run_pytests.py
+++ run_pytests.py
@@ -2,34 +2,36 @@ import json
 import os
 
 import pytest
-import pythonbasictools as pbt
 from pytest_jsonreport.plugin import JSONReport
-from pytest_cov.plugin import CovPlugin
+import argparse
 
 from tests import configs
 from tests.conftest import RUN_SLOW_ARG_NAME
 
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=str, default="tests", help="Path to the tests directory")
+    parser.add_argument("--N_RANDOM_TESTS_PER_CASE", type=int, default=configs.N_RANDOM_TESTS_PER_CASE,
+                        help="Number of random tests per test case")
+    parser.add_argument("--save_report", type=str, default="True", help="Save the report to a file")
+    parser.add_argument("--cov", type=str, default="src", help="Coverage")
+    parser.add_argument("--cov-report", type=str, default="xml", help="Coverage report")
+    parser.add_argument("--durations", type=int, default=10, help="Number of durations")
+    parser.add_argument(f"--{RUN_SLOW_ARG_NAME}", type=str, default="True", help="Run slow tests")
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    sys_args_dict = pbt.cmds.get_cmd_kwargs(
-        {
-            1                        : os.path.join(os.getcwd(), "tests"),
-            "N_RANDOM_TESTS_PER_CASE": configs.N_RANDOM_TESTS_PER_CASE,
-            "save_report"            : "True",
-            "cov"                    : "src",
-            "cov-report"             : "xml",
-            RUN_SLOW_ARG_NAME        : "True",
-            "durations"              : 10,
-        }
-    )
-    sys_args_dict["cov-report"] = sys_args_dict["cov_report"]  # fix
-    configs.N_RANDOM_TESTS_PER_CASE = sys_args_dict["N_RANDOM_TESTS_PER_CASE"]
-    configs.RUN_SLOW_TESTS = 't' in str(sys_args_dict[RUN_SLOW_ARG_NAME]).lower()
+    args = parse_args()
+    configs.N_RANDOM_TESTS_PER_CASE = args.N_RANDOM_TESTS_PER_CASE
+    configs.RUN_SLOW_TESTS = 't' in str(getattr(args, RUN_SLOW_ARG_NAME)).lower()
     json_plugin = JSONReport()
     pytest_main_args_names = ["cov", "cov-report", "durations"]
-    pytest_main_args = [f"--{k}={sys_args_dict[k]}" for k in pytest_main_args_names]
-    pytest.main([sys_args_dict[1], *pytest_main_args], plugins=[json_plugin])
+    pytest_main_args = [f"--cov={args.cov}", f"--cov-report={args.cov_report}", f"--durations={args.durations}"]
+    pytest.main([args.path, *pytest_main_args], plugins=[json_plugin])
     json_path = os.path.join(os.getcwd(), "tests", "tmp", f"tests_report_rn{configs.N_RANDOM_TESTS_PER_CASE}.json")
-    save_report = 't' in str(sys_args_dict["save_report"]).lower()
+    save_report = 't' in str(args.save_report).lower()
     if save_report:
         json_plugin.save_report(json_path)
         json_data = json.load(open(json_path))
