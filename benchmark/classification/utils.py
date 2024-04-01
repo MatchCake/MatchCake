9cbbe2b0b5b87b0ecd7794a252027b4d177a73ee
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2024-04-01 16:33:18 -0400

    Update utils.py

--- benchmark/classification/utils.py
+++ benchmark/classification/utils.py
@@ -1,9 +1,15 @@
-from typing import Sequence
+import functools
+import os
+import warnings
+from collections import defaultdict
+from functools import partial
+from typing import Sequence, Optional, List
 
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, accuracy_score, precision_score, recall_score, f1_score
 
 MPL_RC_DEFAULT_PARAMS = {
     "font.size": 18,
@@ -347,3 +353,266 @@ def number_axes(axes: Sequence[plt.Axes], **kwargs) -> Sequence[plt.Axes]:
                 loc=kwargs.get("loc", "left"),
             )
     return axes
+
+
+def save_on_exit(_method=None, *, save_func_name="to_pickle", save_args=(), **save_kwargs):
+    """
+    Decorator for a method that saves the object on exit.
+
+    :param _method: The method to decorate.
+    :type _method: Callable
+    :param save_func_name: The name of the method that saves the object.
+    :type save_func_name: str
+    :param save_args: The arguments of the save method.
+    :type save_args: tuple
+    :param save_kwargs: The keyword arguments of the save method.
+    :return: The decorated method.
+    """
+
+    def decorator(method):
+        @functools.wraps(method)
+        def wrapper(*args, **kwargs):
+            try:
+                return method(*args, **kwargs)
+            except Exception as e:
+                warnings.warn(f"Failed to run method {method.__name__}: {e}", RuntimeWarning)
+                raise e
+            finally:
+                self = args[0]
+                if not hasattr(self, save_func_name):
+                    warnings.warn(
+                        f"The object {self.__class__.__name__} does not have a save method named {save_func_name}.",
+                        RuntimeWarning
+                    )
+                try:
+                    getattr(self, save_func_name)(*save_args, **save_kwargs)
+                except Exception as e:
+                    warnings.warn(
+                        f"Failed to save object {self.__class__.__name__}: {e} with {save_func_name} method",
+                        RuntimeWarning
+                    )
+                    # raise e
+
+        # wrapper.__name__ = method.__name__ + "@save_on_exit"
+        wrapper.__name__ = method.__name__
+        return wrapper
+
+    if _method is None:
+        return decorator
+    else:
+        return decorator(_method)
+
+
+def get_gram_predictor(cls, kernel, x_train, **kwargs):
+    def predictor(x_test):
+        return cls.predict(kernel.pairwise_distances(x_test, x_train, **kwargs))
+    return predictor
+
+
+class KPredictorContainer:
+    def __init__(self, name: str = ""):
+        self.name = name
+        self.container = defaultdict(dict)
+
+    def get(self, key, inner_key, default_value=None):
+        return self.container.get(key, {}).get(inner_key, default_value)
+
+    def set(self, key, inner_key, value):
+        self.container[key][inner_key] = value
+
+    def get_inner(self, key):
+        return self.container[key]
+
+    def get_outer(self, inner_key, default_value=None):
+        outer_dict = {key: default_value for key in self.container}
+        for key, inner_dict in self.container.items():
+            for _inner_key, value in inner_dict.items():
+                if _inner_key == inner_key:
+                    outer_dict[key] = value
+        return outer_dict
+
+    def __setitem__(self, key, value):
+        if isinstance(key, tuple):
+            return self.set(key[0], key[1], value)
+        elif isinstance(value, dict):
+            self.container[key] = value
+        else:
+            raise ValueError("Key in __setitem__ must be a tuple")
+
+    def __getitem__(self, item):
+        if isinstance(item, tuple):
+            return self.get(item[0], item[1])
+        else:
+            return self.get_inner(item)
+
+    def items(self, default_value=None):
+        for key in self.keys():
+            yield key, self.get(*key, default_value=default_value)
+
+    def keys(self):
+        for key, inner_dict in self.container.items():
+            for inner_key in inner_dict:
+                yield key, inner_key
+
+    def to_dataframe(
+            self,
+            *,
+            outer_column: str = "outer",
+            inner_column: str = "inner",
+            value_column: str = "value",
+            default_value=None,
+    ) -> pd.DataFrame:
+        all_keys = list(self.keys())
+        all_outer_keys = list(set(k[0] for k in all_keys))
+        all_inner_keys = list(set(k[1] for k in all_keys))
+        df_dict = {
+            outer_column: [],
+            inner_column: [],
+            value_column: [],
+        }
+        for ok in all_outer_keys:
+            for ik in all_inner_keys:
+                df_dict[outer_column].append(ok)
+                df_dict[inner_column].append(ik)
+                df_dict[value_column].append(self.get(ok, ik, default_value))
+        return pd.DataFrame(df_dict)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name},outer_keys={list(self.container.keys())})"
+
+    def save_item(self, filepath: str, *key):
+        import pickle
+
+        item = self.__getitem__(key)
+        if item is None:
+            return
+        if not filepath.endswith(".pkl"):
+            filepath += ".pkl"
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        with open(filepath, "wb") as f:
+            pickle.dump(item, f)
+        return self
+
+    def load_item(self, filepath, *key):
+        import pickle
+
+        if not filepath.endswith(".pkl"):
+            filepath += ".pkl"
+        if not os.path.isfile(filepath):
+            return self
+        with open(filepath, "rb") as f:
+            item = pickle.load(f)
+        self.__setitem__(key, item)
+        return self
+
+    def __contains__(self, item):
+        if isinstance(item, tuple):
+            return self.get(item[0], item[1]) is not None
+        else:
+            return item in self.container
+
+    def save_item_to_txt(self, filepath: str, *key):
+        if not filepath.endswith(".txt"):
+            filepath += ".txt"
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        item = self.__getitem__(key)
+        with open(filepath, "w") as f:
+            f.write(str(item))
+
+
+class MetricsContainer:
+    ACCURACY_KEY = "Accuracy"
+    F1_KEY = "F1-Score"
+    PRECISION_KEY = "Precision"
+    RECALL_KEY = "Recall"
+    available_metrics = {
+        ACCURACY_KEY: accuracy_score,
+        F1_KEY: partial(f1_score, average="weighted"),
+        PRECISION_KEY: partial(precision_score, average="weighted"),
+        RECALL_KEY: partial(recall_score, average="weighted"),
+    }
+
+    def __init__(
+            self,
+            metrics: Optional[List[str]] = None,
+            *,
+            pre_name: str = "",
+            post_name: str = "",
+            name_separator: str = "_",
+    ):
+        self.metrics = metrics or list(self.available_metrics.keys())
+        self.pre_name = pre_name
+        self.post_name = post_name
+        self.name_separator = name_separator
+        if pre_name:
+            pre_name += name_separator
+        if post_name:
+            post_name = name_separator + post_name
+        self.metrics_names = [
+            f"{pre_name}{name}{post_name}" for name in self.metrics
+        ]
+        self.containers = {
+            metric: KPredictorContainer(metric_name)
+            for metric, metric_name in zip(self.metrics, self.metrics_names)
+        }
+
+    @property
+    def containers_list(self):
+        return list(self.containers.values())
+
+    def get_is_metrics_all_computed(self, key, inner_key):
+        return all(self.get(metric, key, inner_key, None) is not None for metric in self.metrics)
+
+    def get(self, metric: str, key, inner_key, default_value=None):
+        return self.containers[metric].get(key, inner_key, default_value=default_value)
+
+    def set(self, metric: str, key, inner_key, value):
+        self.containers[metric].set(key, inner_key, value)
+
+    def get_metric(self, metric: str):
+        return self.containers[metric]
+
+    def compute_metrics(self, y_true, y_pred, key, inner_key, **kwargs):
+        for metric in self.metrics:
+            self.set(metric, key, inner_key, self.available_metrics[metric](y_true, y_pred))
+        return self.containers
+
+    def get_item_metrics(self, *key, default_value=None):
+        return {
+            metric: self.containers[metric].__getitem__(key)
+            if self.containers[metric].__contains__(key) else default_value
+            for metric in self.metrics
+        }
+
+    def save_item_metrics(self, filepath: str, *key):
+        import pickle
+
+        to_save = self.get_item_metrics(*key)
+        if not filepath.endswith(".pkl"):
+            filepath += ".pkl"
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        with open(filepath, "wb") as f:
+            pickle.dump(to_save, f)
+        return self
+
+    def load_item_metrics(self, filepath: str, *key):
+        import pickle
+
+        if not filepath.endswith(".pkl"):
+            filepath += ".pkl"
+        if not os.path.isfile(filepath):
+            return self
+        with open(filepath, "rb") as f:
+            item = pickle.load(f)
+        for metric, value in item.items():
+            self.set(metric, *key, value)
+        return self
+
+    def save_item_metrics_to_txt(self, filepath: str, *key):
+        if not filepath.endswith(".txt"):
+            filepath += ".txt"
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        item = self.get_item_metrics(*key)
+        with open(filepath, "w") as f:
+            f.write(str(item))
+
