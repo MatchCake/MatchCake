771b1d8430fdee67e06b6fd3034646103e17ab2a
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2024-04-03 19:39:36 -0400

    Create torch_utils.py

---
+++ src/matchcake/utils/torch_utils.py
@@ -0,0 +1,41 @@
+import numbers
+from typing import Any
+
+import numpy as np
+import torch
+
+
+def to_tensor(x: Any, dtype=torch.float32):
+    if isinstance(x, np.ndarray):
+        return torch.from_numpy(x).type(dtype)
+    elif isinstance(x, torch.Tensor):
+        return x.type(dtype)
+    elif isinstance(x, numbers.Number):
+        return torch.tensor(x, dtype=dtype)
+    elif isinstance(x, dict):
+        return {k: to_tensor(v, dtype=dtype) for k, v in x.items()}
+    elif isinstance(x, (list, tuple)):
+        return type(x)([to_tensor(v, dtype=dtype) for v in x])
+    elif not isinstance(x, torch.Tensor):
+        try:
+            return torch.tensor(x, dtype=dtype)
+        except Exception as e:
+            raise ValueError(f"Unsupported type {type(x)}") from e
+    raise ValueError(f"Unsupported type {type(x)}")
+
+
+def to_numpy(x: Any, dtype=np.float32):
+    if isinstance(x, np.ndarray):
+        return np.asarray(x, dtype=dtype)
+    elif isinstance(x, torch.Tensor):
+        return x.detach().cpu().numpy()
+    elif isinstance(x, numbers.Number):
+        return x
+    elif isinstance(x, dict):
+        return {k: to_numpy(v, dtype=dtype) for k, v in x.items()}
+    elif not isinstance(x, torch.Tensor):
+        try:
+            return np.asarray(x, dtype=dtype)
+        except Exception as e:
+            raise ValueError(f"Unsupported type {type(x)}") from e
+    raise ValueError(f"Unsupported type {type(x)}")
