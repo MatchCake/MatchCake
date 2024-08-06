b6a0b8741c6aec9c167a31fefe6bc4232edbb5e3
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2024-08-06 10:29:38 -0400

    Update _pfaffian.py

--- src/matchcake/utils/_pfaffian.py
+++ src/matchcake/utils/_pfaffian.py
@@ -1,113 +1,9 @@
-from typing import Union
+from typing import Union, Literal
 import pennylane as qml
-import pennylane.numpy as pnp
 import numpy as np
 import warnings
 from .math import convert_and_cast_like
-
-
-def pfaffian_ltl(__matrix, overwrite_input=False) -> Union[float, complex]:
-    r"""
-    Compute the Pfaffian of a real or complex skew-symmetric
-    matrix A (A=-A^T). If overwrite_a=True, the matrix A
-    is overwritten in the process. This function uses
-    the Parlett-Reid algorithm.
-
-    This code is adapted of the function `pfaffian_LTL`
-    from https://github.com/basnijholt/pfapack/blob/master/pfapack/pfaffian.py.
-
-    :param __matrix: Matrix to compute the Pfaffian of
-    :type __matrix: np.ndarray
-    :param overwrite_input: Whether to overwrite the input matrix
-    :type overwrite_input: bool
-    :return: Pfaffian of the matrix
-    :rtype: Union[float, complex]
-    """
-    if overwrite_input:
-        matrix = __matrix
-    else:
-        matrix = qml.math.ones_like(__matrix) * __matrix
-    shape = qml.math.shape(matrix)
-    # Check if matrix is square
-    assert shape[-2] == shape[-1] > 0
-    # Check if it's skew-symmetric
-    matrix_t = qml.math.einsum("...ij->...ji", matrix)
-    assert qml.math.allclose(matrix, -matrix_t)
-
-    n, m = shape[-2:]
-    # Quick return if possible
-    if n % 2 == 1:
-        return 0.0
-    matrix = qml.math.cast(matrix, dtype=complex)
-    zero_like = convert_and_cast_like(0, matrix)
-    pfaffian_val = qml.math.convert_like(pnp.ones(shape[:-2], dtype=complex), matrix)
-    
-    for k in range(0, n - 1, 2):
-        # First, find the largest entry in A[k+1:,k] and
-        # permute it to A[k+1,k]
-        kp = k + 1 + qml.math.abs(matrix[..., k + 1:, k]).argmax(axis=-1)
-        
-        # Check if we need to pivot
-        # pivot_condition = ~qml.math.isclose(kp, k + 1)
-        # if qml.math.all(pivot_condition):
-        # if kp != k + 1:  # TODO: need to add batch support
-            # interchange rows k+1 and kp
-            # temp = qml.math.ones_like(matrix[..., k + 1, k:]) * matrix[..., k + 1, k:]
-            # matrix[..., k + 1, k:] = matrix[..., kp, k:]
-            # matrix[..., kp, k:] = temp
-            # matrix = _pivot_rows(matrix, k, kp)
-
-            # Then interchange columns k+1 and kp
-            # temp = qml.math.ones_like(matrix[..., k:, k + 1]) * matrix[..., k:, k + 1]
-            # matrix[..., k:, k + 1] = matrix[..., k:, kp]
-            # matrix[..., k:, kp] = temp
-            # matrix = _pivot_cols(matrix, k, kp)
-            # matrix = _pivot(matrix, k, kp)
-
-            # every interchange corresponds to a "-" in det(P)
-            # pfaffian_val *= -1
-            # pfaffian_val *= qml.math.where(pivot_condition, -1, 1)
-        
-        # if qml.math.isclose(matrix[..., k + 1, k], zero_like):
-        #     # if we encounter a zero on the super/subdiagonal, the Pfaffian is 0
-        #     return zero_like
-        # else:
-        #     # Now form the Gauss vector
-        #     tau = qml.math.ones_like(matrix[..., k, k + 2:]) * matrix[..., k, k + 2:]
-        #     zero_mask = qml.math.isclose(matrix[..., k, k + 1], zero_like)
-        #     tau = tau / matrix[..., k, k + 1]
-        #     tau = qml.math.where(zero_mask, zero_like, tau)
-        #     pfaffian_val *= matrix[..., k, k + 1]
-        #
-        #     if k + 2 < n:
-        #         # Update the matrix block A(k+2:,k+2)
-        #         matrix[..., k + 2:, k + 2:] = matrix[..., k + 2:, k + 2:] + qml.math.outer(
-        #             tau, matrix[..., k + 2:, k + 1]
-        #         )
-        #         matrix[..., k + 2:, k + 2:] = matrix[..., k + 2:, k + 2:] - qml.math.outer(
-        #             matrix[..., k + 2:, k + 1], tau
-        #         )
-
-        # Check if we need to pivot
-        pivot_condition = ~qml.math.isclose(kp, k + 1)
-        # interchange rows and cols k+1 and kp (pivot if needed)
-        matrix = qml.math.where(pivot_condition[..., None, None], _pivot(matrix, k, kp), matrix)
-        # every interchange corresponds to a "-" in det(P)
-        pfaffian_val *= qml.math.where(pivot_condition, -1, 1)
-        #
-        if qml.math.isclose(matrix[..., k + 1, k], zero_like):
-            # if we encounter a zero on the super/subdiagonal, the Pfaffian is 0
-            pfaffian_val *= zero_like
-        else:
-            pfaffian_val *= matrix[..., k, k + 1]
-
-        if k + 2 < n:
-            # Now form the Gauss vector
-            tau = _compute_gauss_vector(matrix, k)
-            # Update the matrix block A(k+2:,k+2)
-            matrix = _update_matrix_block_kp2_kp2(matrix, k, tau)
-    
-    return pfaffian_val
+from ..templates import TensorLike
 
 
 def _pivot(__matrix, k, kp):
@@ -142,13 +38,21 @@ def _compute_gauss_vector(__matrix, k):
 
 
 def _update_matrix_block_kp2_kp2(__matrix, k, tau):
-    matrix = qml.math.ones_like(__matrix) * __matrix
-    matrix[..., k + 2:, k + 2:] += qml.math.einsum("...i,...j->...ij", tau, matrix[..., k + 2:, k + 1])
-    matrix[..., k + 2:, k + 2:] -= qml.math.einsum("...i,...j->...ij", matrix[..., k + 2:, k + 1], tau)
-    return matrix
+    add_matrix = qml.math.zeros_like(__matrix)
+    add_subb_matrix = (
+        qml.math.einsum("...i,...j->...ij", tau, __matrix[..., k + 2:, k + 1])
+        -
+        qml.math.einsum("...i,...j->...ij", __matrix[..., k + 2:, k + 1], tau)
+    )
+    add_matrix[..., k + 2:, k + 2:] += add_subb_matrix
+    return __matrix + add_matrix
 
 
-def _batch_pfaffian_ltl(__matrix, overwrite_input=False, test_input: bool = False) -> Union[float, complex]:
+def batch_pfaffian_ltl(
+        __matrix: TensorLike,
+        overwrite_input: bool = False,
+        test_input: bool = False
+) -> Union[float, complex, TensorLike]:
     r"""
     Compute the Pfaffian of a real or complex skew-symmetric
     matrix A (A=-A^T). If overwrite_a=True, the matrix A
@@ -159,11 +63,14 @@ def _batch_pfaffian_ltl(__matrix, overwrite_input=False, test_input: bool = Fals
     from https://github.com/basnijholt/pfapack/blob/master/pfapack/pfaffian.py.
 
     :param __matrix: Matrix to compute the Pfaffian of
-    :type __matrix: np.ndarray
+    :type __matrix: TensorLike
     :param overwrite_input: Whether to overwrite the input matrix
     :type overwrite_input: bool
+    :param test_input: Whether to test the input matrix for skew-symmetry
+    :type test_input: bool
+
     :return: Pfaffian of the matrix
-    :rtype: Union[float, complex]
+    :rtype: Union[float, complex, TensorLike]
     """
     if overwrite_input:
         matrix = __matrix
@@ -184,14 +91,15 @@ def _batch_pfaffian_ltl(__matrix, overwrite_input=False, test_input: bool = Fals
 
     # Quick return if possible
     if n % 2 == 1:
-        return qml.math.zeros_like(pfaffian_val)
+        return pfaffian_val * zero_like * matrix[..., 0, 0]  # 0.0 but with require grad if needed
 
     for k in range(0, n - 1, 2):
         # First, find the largest entry in A[k+1:,k] and permute it to A[k+1,k]
         kp = k + 1 + qml.math.abs(matrix[..., k + 1:, k]).argmax(-1)
+        kp1 = qml.math.convert_like(k + 1, kp)
 
         # Check if we need to pivot
-        pivot_condition = ~qml.math.isclose(kp, k + 1)
+        pivot_condition = ~qml.math.isclose(kp, kp1)
         # interchange rows and cols k+1 and kp (pivot if needed)
         matrix = qml.math.where(pivot_condition[..., None, None], _pivot(matrix, k, kp), matrix)
         # every interchange corresponds to a "-" in det(P)
@@ -210,21 +118,12 @@ def _batch_pfaffian_ltl(__matrix, overwrite_input=False, test_input: bool = Fals
     return pfaffian_val
 
 
-def batch_pfaffian_ltl(__matrix, overwrite_input=False) -> Union[float, complex]:
-    ndim = qml.math.ndim(__matrix)
-    if ndim == 2:
-        return pfaffian_ltl(__matrix, overwrite_input)
-    elif ndim == 3:
-        # TODO: need to add batch support
-        return qml.math.stack([
-            pfaffian_ltl(__matrix[i], overwrite_input)
-            for i in range(qml.math.shape(__matrix)[0])
-        ])
-    else:
-        raise ValueError(f"Invalid ndim. Got {ndim}, must be 2 or 3.")
-
-
-def pfaffian(__matrix, overwrite_input=False, method="P") -> Union[float, complex]:
+def pfaffian(
+        __matrix: TensorLike,
+        overwrite_input: bool = False,
+        method: Literal["P", "H", "det", "bLTL"] = "bLTL",
+        epsilon: float = 1e-12
+) -> Union[float, complex, TensorLike]:
     """pfaffian(A, overwrite_a=False, method='P')
 
     Compute the Pfaffian of a real or complex skew-symmetric
@@ -237,19 +136,28 @@ def pfaffian(__matrix, overwrite_input=False, method="P") -> Union[float, comple
     from https://github.com/basnijholt/pfapack/blob/master/pfapack/pfaffian.py.
 
     :param __matrix: Matrix to compute the Pfaffian of
-    :type __matrix: np.ndarray
+    :type __matrix: TensorLike
     :param overwrite_input: Whether to overwrite the input matrix
     :type overwrite_input: bool
-    :param method: Method to use. Either 'P' or 'H'.
-    :type method: str
+    :param method: Method to use. 'P' for Parlett-Reid algorithm, 'H' for Householder tridiagonalization,
+        'det' for determinant, 'bLTL' for batched Parlett-Reid algorithm
+    :type method: Literal["P", "H", "det", "bLTL"]
+    :param epsilon: Tolerance for the determinant method
+    :type epsilon: float
     :return: Pfaffian of the matrix
-    :rtype: Union[float, complex]
+    :rtype: Union[float, complex, TensorLike]
     """
     shape = qml.math.shape(__matrix)
     assert shape[-2] == shape[-1] > 0
     
     if method == "P":
-        return batch_pfaffian_ltl(__matrix, overwrite_input)
+        from pfapack.pfaffian import pfaffian
+        warnings.warn(
+            "The method 'H' is not implemented yet. "
+            "It is recommended to use the method 'P' instead.",
+            UserWarning,
+        )
+        return pfaffian(__matrix, overwrite_input, method="P")
     elif method == "H":
         from pfapack.pfaffian import pfaffian_householder
         warnings.warn(
@@ -261,9 +169,9 @@ def pfaffian(__matrix, overwrite_input=False, method="P") -> Union[float, comple
     elif method == "det":
         backend = qml.math.get_interface(__matrix)
         if backend in ["autograd", "numpy"]:
-            return qml.math.sqrt(qml.math.abs(qml.math.linalg.det(__matrix)))
-        return qml.math.sqrt(qml.math.abs(qml.math.det(__matrix)))
+            return qml.math.sqrt(qml.math.abs(qml.math.linalg.det(__matrix)) + epsilon)
+        return qml.math.sqrt(qml.math.abs(qml.math.det(__matrix)) + epsilon)
     elif method == "bLTL":
-        return _batch_pfaffian_ltl(__matrix, overwrite_input)
+        return batch_pfaffian_ltl(__matrix, overwrite_input)
     else:
         raise ValueError(f"Invalid method. Got {method}, must be 'P' or 'H'.")
