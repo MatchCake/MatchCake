11ce0bc4213ea9bf9fa57691e40894afd14207ee
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2024-10-23 21:42:19 -0400

    Update qubit_by_qubit_sampling.py

--- src/matchcake/devices/sampling_strategies/qubit_by_qubit_sampling.py
+++ src/matchcake/devices/sampling_strategies/qubit_by_qubit_sampling.py
@@ -8,6 +8,7 @@ import pennylane as qml
 from ...utils.torch_utils import to_numpy
 from ...utils.math import random_index
 from ..probability_strategies import ProbabilityStrategy
+import tqdm
 
 
 class QubitByQubitSampling(SamplingStrategy):
@@ -87,18 +88,24 @@ class QubitByQubitSampling(SamplingStrategy):
 
         :return: Samples with shape (shots, num_wires) of probabilities |<x|psi>|^2
         """
+        p_bar = tqdm.tqdm(
+            total=device.num_wires,
+            desc=f"[{self.NAME}] Generating Samples",
+            disable=not kwargs.get("show_progress", False),
+            unit="wires",
+        )
         # pi_0 = pi_0(x_0) = [p_0(0), p_0(1)]
         probs = to_numpy(states_prob_func(np.asarray([[0], [1]]), Wires([0])))
         # Sample x_0 from the probability distribution pi_0(x_0)
         samples = random_index(probs, n=device.shots, axis=-1)
         batch_shape = qml.math.shape(samples)
+        p_bar.update(1)
 
         # x_0
         binary = device.states_to_binary(samples, 1)
         binaries = [binary]
         for j in range(1, device.num_wires):
             binaries_array = qml.math.concatenate(binaries, -1)
-            # pi_j = pi_j(x_0, ..., x_{j-1}, x_j) / pi_{j-1}(x_0, ..., x_{j-1})
             unique_states = np.unique(binaries_array.reshape(-1, binaries_array.shape[-1]), axis=0)
             unique_batch_shape = unique_states.shape[:-1]
 
@@ -107,8 +114,8 @@ class QubitByQubitSampling(SamplingStrategy):
             ones_states = qml.math.concatenate([unique_states, qml.math.full((*unique_batch_shape, 1), 1)], -1)
             all_states = qml.math.stack([zeros_states, ones_states], axis=0).reshape(-1, j + 1).astype(int)
 
-            # compute probs of unique states
-            unique_states_probs = states_prob_func(all_states, np.arange(j + 1))
+            # compute probs of unique states: pi_j = pi_j(x_0, ..., x_{j-1}, x_j) / pi_{j-1}(x_0, ..., x_{j-1})
+            unique_states_probs = to_numpy(states_prob_func(all_states, np.arange(j + 1)))
 
             # compute the probs tensor
             probs = qml.math.full((*batch_shape, 2), 0.0)
@@ -116,17 +123,14 @@ class QubitByQubitSampling(SamplingStrategy):
                 mask = np.isclose(binaries_array, state[:-1]).all(axis=-1)
                 probs[..., state[-1]] = np.where(mask, unique_states_probs[..., i], probs[..., state[-1]])
 
-            if np.isnan(probs).any():
-                raise ValueError("Something went wrong in the computation of the probs tensor.")
-
-            # split the first dimension into two parts
             # Sample x_j from the probability distribution pi_j
             samples = random_index(probs, axis=-1)
-            # samples = random_index(probs[0], n=device.shots, axis=-1)
 
             # x_j
             binary = device.states_to_binary(samples, 1)
             binaries.append(binary)
+            p_bar.update(1)
+        p_bar.close()
 
         # return x_0 ... x_{n-1}
         return qml.math.concatenate(binaries, -1)
