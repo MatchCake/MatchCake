0768173f0dc39dbf97ae05d1e188e3d42e79d224
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2025-02-05 23:32:05 -0500

    Update test_expectation_values_pauli_strings.py

--- tests/test_nif_device/test_expectation_values_pauli_strings.py
+++ tests/test_nif_device/test_expectation_values_pauli_strings.py
@@ -77,3 +77,28 @@ def test_nif_pauli_strings_on_basis_state_against_qubit_device_outer_sum(basis_s
         atol=ATOL_APPROX_COMPARISON,
         rtol=RTOL_APPROX_COMPARISON
     )
+
+
+@pytest.mark.parametrize(
+    "basis_state,pauli_string",
+    hand_made_test_data
+)
+def test_nif_pauli_strings_on_basis_state_against_qubit_device_probs(basis_state, pauli_string):
+    def circuit():
+        qml.BasisState(np.asarray(basis_state), wires=np.arange(len(basis_state)))
+        return qml.probs()
+
+    nif_device, qubit_device = devices_init(
+        wires=len(basis_state), shots=None, contraction_strategy=None, name="lightning.qubit"
+    )
+    nif_qnode = qml.QNode(circuit, nif_device)
+    qubit_qnode = qml.QNode(circuit, qubit_device)
+    expected_value = qubit_qnode()
+    actual_value = nif_qnode()
+
+    np.testing.assert_allclose(
+        actual_value,
+        expected_value,
+        atol=ATOL_APPROX_COMPARISON,
+        rtol=RTOL_APPROX_COMPARISON
+    )
