536f998a0c5effe11ae564866753d01b634384f1
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2025-02-18 20:25:29 -0500

    Create test_gradients_expval_pauli_strings.py

---
+++ tests/test_nif_device/test_gradients/test_gradients_expval_pauli_strings.py
@@ -0,0 +1,115 @@
+import numpy as np
+import pennylane as qml
+import pytest
+import torch
+
+from matchcake import BatchHamiltonian
+from matchcake.devices.contraction_strategies import contraction_strategy_map
+from torch.autograd import gradcheck
+
+from matchcake.operations import Rxx
+from .. import devices_init
+from ... import get_slow_test_mark
+from ...configs import (
+    N_RANDOM_TESTS_PER_CASE,
+    TEST_SEED,
+    ATOL_APPROX_COMPARISON,
+    RTOL_APPROX_COMPARISON,
+    set_seed,
+)
+
+set_seed(TEST_SEED)
+
+
+hand_made_test_data = [
+    ([0, 0], [qml.PauliZ(0), qml.PauliZ(1)]),
+    ([0, 0], [qml.PauliZ(0) @ qml.PauliZ(1)]),
+    ([0, 1], [qml.PauliZ(0) @ qml.PauliZ(1)]),
+    ([1, 0], [qml.PauliZ(0) @ qml.PauliZ(1)]),
+    ([1, 1], [qml.PauliZ(0) @ qml.PauliZ(1)]),
+    ([0, 0, 0], [qml.PauliZ(0) @ qml.PauliZ(1)]),
+    ([0, 0, 0], [qml.PauliZ(0) @ qml.PauliZ(1), qml.PauliZ(0) @ qml.PauliZ(1)]),
+    ([0, 0, 0], [qml.PauliZ(0) @ qml.PauliZ(1), qml.PauliZ(1) @ qml.PauliZ(2)]),
+    ([0, 1, 0], [qml.PauliZ(0) @ qml.PauliZ(1), qml.PauliZ(1) @ qml.PauliZ(2)]),
+    ([1, 0, 0], [qml.PauliZ(0) @ qml.PauliZ(1), qml.PauliZ(1) @ qml.PauliZ(2)]),
+    ([1, 1, 0], [qml.PauliZ(0) @ qml.PauliZ(1), qml.PauliZ(1) @ qml.PauliZ(2)]),
+    ([0, 0, 1], [qml.PauliZ(0) @ qml.PauliZ(1), qml.PauliZ(1) @ qml.PauliZ(2)]),
+    ([0, 1, 1], [qml.PauliZ(0) @ qml.PauliZ(1), qml.PauliZ(1) @ qml.PauliZ(2)]),
+    ([1, 0, 1], [qml.PauliZ(0) @ qml.PauliZ(1), qml.PauliZ(1) @ qml.PauliZ(2)]),
+    ([1, 1, 1], [qml.PauliZ(0) @ qml.PauliZ(1), qml.PauliZ(1) @ qml.PauliZ(2)]),
+    ([1, 1, 1], [qml.PauliX(0) @ qml.PauliX(1), qml.PauliZ(1) @ qml.PauliZ(2)]),
+    ([1, 1, 1], [qml.PauliX(0) @ qml.PauliZ(1), qml.PauliX(1) @ qml.PauliZ(2)]),
+    ([1, 1, 1], [qml.PauliX(0) @ qml.PauliZ(1), qml.PauliY(1) @ qml.PauliZ(2)]),
+]
+
+rn_test_data = [
+    (
+            np.random.choice([0, 1], size=n_wires),
+            [
+                p0(w) @ p1(w+np.random.randint(1, n_wires-w))
+                for w in np.random.randint(n_wires-1, size=n_wires)
+            ],
+            contraction_strategy,
+    )
+    for n_wires in range(2, 6)
+    for i in range(N_RANDOM_TESTS_PER_CASE)
+    for contraction_strategy in contraction_strategy_map.keys()
+    for p0 in [qml.PauliX, qml.PauliY, qml.PauliZ]
+    for p1 in [qml.PauliX, qml.PauliY, qml.PauliZ]
+]
+
+
+@pytest.mark.parametrize(
+    "basis_state,pauli_string",
+    hand_made_test_data
+)
+def test_nif_pauli_strings_grads_with_random_circuit_against_torch_gradcheck_handmade_batch_hamiltonian(
+        basis_state, pauli_string
+):
+    nif_device, _ = devices_init(
+        wires=len(basis_state), shots=None, contraction_strategy=None, name="lightning.qubit"
+    )
+    hamiltonian = BatchHamiltonian(np.ones(len(pauli_string)), pauli_string)
+
+    @qml.qnode(nif_device)
+    def circuit(params):
+        qml.BasisState(np.asarray(basis_state), wires=np.arange(len(basis_state)))
+        Rxx(params, wires=[0, 1])
+        return qml.expval(hamiltonian)
+
+    init_param = np.pi / 7
+    init_params_nif = torch.Tensor([init_param, ]).requires_grad_()
+    assert gradcheck(
+        circuit, (init_params_nif,),
+        eps=1e-3,
+        atol=ATOL_APPROX_COMPARISON,
+        rtol=RTOL_APPROX_COMPARISON
+    )
+
+
+@pytest.mark.parametrize(
+    "basis_state,pauli_string",
+    hand_made_test_data
+)
+def test_nif_pauli_strings_grads_with_random_circuit_against_torch_gradcheck_handmade_sum_hamiltonian(
+        basis_state, pauli_string
+):
+    nif_device, _ = devices_init(
+        wires=len(basis_state), shots=None, contraction_strategy=None, name="lightning.qubit"
+    )
+    hamiltonian = sum(pauli_string)
+
+    @qml.qnode(nif_device)
+    def circuit(params):
+        qml.BasisState(np.asarray(basis_state), wires=np.arange(len(basis_state)))
+        Rxx(params, wires=[0, 1])
+        return qml.expval(hamiltonian)
+
+    init_param = np.pi / 7
+    init_params_nif = torch.Tensor([init_param, ]).requires_grad_()
+    assert gradcheck(
+        circuit, (init_params_nif,),
+        eps=1e-3,
+        atol=ATOL_APPROX_COMPARISON,
+        rtol=RTOL_APPROX_COMPARISON
+    )
