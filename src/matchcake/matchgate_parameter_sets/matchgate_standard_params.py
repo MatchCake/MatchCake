0ec33b6209d39b7893c816f4d0dbd28c01ca2da2
Jeremie Gince <jeremie.gince@usherbrooke.ca>
2025-11-12 17:23:24 -0500

    Refactor MatchgateStandardParams to use dataclass and integrate tensor support

--- src/matchcake/matchgate_parameter_sets/matchgate_standard_params.py
+++ src/matchcake/matchgate_parameter_sets/matchgate_standard_params.py
@@ -1,12 +1,19 @@
-from typing import Union
+from typing import Optional
 
 import numpy as np
 import pennylane as qml
-from pennylane import numpy as pnp
+import torch
+from dataclasses import dataclass
+from pennylane.typing import TensorLike
 
-from .matchgate_params import MatchgateParams
+from matchcake.utils.torch_utils import to_tensor
 
 
+@dataclass
+class MatchgateParams:
+    ...
+
+@dataclass
 class MatchgateStandardParams(MatchgateParams):
     r"""
     Matchgate standard parameters.
@@ -23,127 +30,71 @@ class MatchgateStandardParams(MatchgateParams):
             \end{bmatrix}
 
         where :math:`a, b, c, d, w, x, y, z` are the parameters.
-
     """
-
-    N_PARAMS = 8
-    ALLOW_COMPLEX_PARAMS = True
-    DEFAULT_RANGE_OF_PARAMS = (-1e12, 1e12)
-    DEFAULT_PARAMS_TYPE = complex
-    ZEROS_INDEXES = [
-        (0, 1),
-        (0, 2),
-        (1, 0),
-        (1, 3),
-        (2, 0),
-        (2, 3),
-        (3, 1),
-        (3, 2),
-    ]
-    ELEMENTS_INDEXES = [
-        (0, 0),
-        (0, 3),  # a, b
-        (3, 0),
-        (3, 3),  # c, d
-        (1, 1),
-        (1, 2),  # w, x
-        (2, 1),
-        (2, 2),  # y, z
-    ]
-    ATTRS = ["a", "b", "c", "d", "w", "x", "y", "z"]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    @property
-    def a(self) -> Union[float, complex]:
-        return self._a
-
-    @property
-    def b(self) -> Union[float, complex]:
-        return self._b
-
-    @property
-    def c(self) -> Union[float, complex]:
-        return self._c
-
-    @property
-    def d(self) -> Union[float, complex]:
-        return self._d
-
-    @property
-    def w(self) -> Union[float, complex]:
-        return self._w
-
-    @property
-    def x(self) -> Union[float, complex]:
-        return self._x
-
-    @property
-    def y(self) -> Union[float, complex]:
-        return self._y
-
-    @property
-    def z(self) -> Union[float, complex]:
-        return self._z
+    a: TensorLike
+    b: TensorLike
+    c: TensorLike
+    d: TensorLike
+    w: TensorLike
+    x: TensorLike
+    y: TensorLike
+    z: TensorLike
 
     @classmethod
-    def to_sympy(cls):
-        import sympy as sp
+    def from_sub_matrices(
+            cls,
+            outer_matrix: TensorLike,
+            inner_matrix: TensorLike,
+    ):
+        return cls(
+            a=outer_matrix[..., 0, 0],
+            b=outer_matrix[..., 0, 1],
+            c=outer_matrix[..., 1, 0],
+            d=outer_matrix[..., 1, 1],
+            w=inner_matrix[..., 0, 0],
+            x=inner_matrix[..., 0, 1],
+            y=inner_matrix[..., 1, 0],
+            z=inner_matrix[..., 1, 1],
+        )
 
-        a, b, c, d = sp.symbols("a b c d")
-        w, x, y, z = sp.symbols("w x y z")
-        return sp.Matrix([a, b, c, d, w, x, y, z])
+    def __post_init__(self):
+        self._check_is_matchgate()
 
-    def to_outer_matrix(self):
-        matrix = self.to_matrix()
-        batched_matrix = qml.math.reshape(matrix, (-1, *matrix.shape[-2:]))
-        outer_matrix = pnp.zeros((batched_matrix.shape[0], 2, 2), dtype=self.DEFAULT_PARAMS_TYPE)
-        outer_matrix[..., 0, 0] = batched_matrix[..., 0, 0]
-        outer_matrix[..., 0, 1] = batched_matrix[..., 0, 3]
-        outer_matrix[..., 1, 0] = batched_matrix[..., 3, 0]
-        outer_matrix[..., 1, 1] = batched_matrix[..., 3, 3]
-        if qml.math.ndim(matrix) == 2:
-            outer_matrix = outer_matrix[0]
-        return outer_matrix
-
-    def to_inner_matrix(self):
-        matrix = self.to_matrix()
-        batched_matrix = qml.math.reshape(matrix, (-1, *matrix.shape[-2:]))
-        inner_matrix = pnp.zeros((batched_matrix.shape[0], 2, 2), dtype=self.DEFAULT_PARAMS_TYPE)
-        inner_matrix[..., 0, 0] = batched_matrix[..., 1, 1]
-        inner_matrix[..., 0, 1] = batched_matrix[..., 1, 2]
-        inner_matrix[..., 1, 0] = batched_matrix[..., 2, 1]
-        inner_matrix[..., 1, 1] = batched_matrix[..., 2, 2]
-        if qml.math.ndim(matrix) == 2:
-            inner_matrix = inner_matrix[0]
-        return inner_matrix
-
-    @classmethod
-    def from_sub_matrices(cls, outer_matrix: np.ndarray, inner_matrix: np.ndarray):
-        o_ndim, i_ndim = qml.math.ndim(outer_matrix), qml.math.ndim(inner_matrix)
-        o_shape, i_shape = qml.math.shape(outer_matrix), qml.math.shape(inner_matrix)
-        if o_shape != i_shape:
-            raise ValueError(f"Expected outer_matrix.shape == inner_matrix.shape, got {o_shape} != {i_shape}.")
-        if o_ndim not in [2, 3]:
-            raise ValueError(f"Expected outer_matrix.ndim in [2, 3], got {o_ndim}.")
-        if o_shape[-2:] != (2, 2):
-            raise ValueError(f"Expected outer_matrix of shape (2, 2), got {o_shape[-2:]}.")
-        batch_size = o_shape[0] if o_ndim == 3 else 1
-        matrix = pnp.zeros((batch_size, 4, 4), dtype=cls.DEFAULT_PARAMS_TYPE)
-        matrix[..., 0, 0] = outer_matrix[..., 0, 0]
-        matrix[..., 0, 3] = outer_matrix[..., 0, 1]
-        matrix[..., 1, 1] = inner_matrix[..., 0, 0]
-        matrix[..., 1, 2] = inner_matrix[..., 0, 1]
-        matrix[..., 2, 1] = inner_matrix[..., 1, 0]
-        matrix[..., 2, 2] = inner_matrix[..., 1, 1]
-        matrix[..., 3, 0] = outer_matrix[..., 1, 0]
-        matrix[..., 3, 3] = outer_matrix[..., 1, 1]
-        if o_ndim == 2:
+    def matrix(
+            self,
+            dtype: torch.dtype = torch.complex128,
+            device: Optional[torch.device] = None,
+    ):
+        shapes = [
+            qml.math.shape(p)
+            for p in [self.a, self.b, self.c, self.d, self.w, self.x, self.y, self.z]
+            if p is not None
+        ]
+        batch_sizes = list(set([s[0] for s in shapes if len(s) > 0]))
+        assert len(batch_sizes) <= 1, f"Expect the same batch size for every parameters. Got: {batch_sizes}."
+        batch_size = batch_sizes[0] if len(batch_sizes) > 0 else 1
+        a, b, c, d, w, x, y, z = [
+            (
+                to_tensor(p, dtype=dtype, device=device)
+                if p is not None
+                else torch.zeros((batch_size,), dtype=dtype, device=device)
+            )
+            for p in [self.a, self.b, self.c, self.d, self.w, self.x, self.y, self.z]
+        ]
+        matrix = torch.zeros((batch_size, 4, 4), dtype=dtype, device=device)
+        matrix[..., 0, 0] = a
+        matrix[..., 0, 3] = b
+        matrix[..., 3, 0] = c
+        matrix[..., 3, 3] = d
+        matrix[..., 1, 1] = w
+        matrix[..., 1, 2] = x
+        matrix[..., 2, 1] = y
+        matrix[..., 2, 2] = z
+        if len(batch_sizes) == 0:
             matrix = matrix[0]
-        return cls.from_matrix(matrix)
+        return matrix
 
-    def adjoint(self):
+    def adjoint(self) -> "MatchgateStandardParams":
         r"""
         Return the adjoint version of the parameters.
 
@@ -160,15 +111,28 @@ class MatchgateStandardParams(MatchgateParams):
             z=qml.math.conjugate(self.z),
         )
 
-    def __matmul__(self, other):
-        if not isinstance(other, MatchgateStandardParams):
-            other = MatchgateStandardParams.parse_from_params(other)
-        a = self.a * other.a + self.b * other.c
-        b = self.a * other.b + self.b * other.d
-        c = self.c * other.a + self.d * other.c
-        d = self.c * other.b + self.d * other.d
-        w = self.w * other.w + self.x * other.y
-        x = self.w * other.x + self.x * other.z
-        y = self.y * other.w + self.z * other.y
-        z = self.y * other.x + self.z * other.z
-        return MatchgateStandardParams(a=a, b=b, c=c, d=d, w=w, x=x, y=y, z=z)
+    def _check_m_m_dagger_constraint(self) -> bool:
+        with torch.no_grad():
+            m_m_dagger = torch.einsum("...ij,...kj->...ik", self.matrix(), torch.conj(self.matrix()))
+            expected_zero = m_m_dagger - torch.eye(4)
+            return torch.allclose(expected_zero, torch.zeros_like(expected_zero), atol=1e-5)
+
+    def _check_m_dagger_m_constraint(self) -> bool:
+        with torch.no_grad():
+            m_dagger_m = torch.einsum("...ji,...jk->...ik", torch.conj(self.matrix()), self.matrix())
+            expected_zero = m_dagger_m - torch.eye(4)
+            return torch.allclose(expected_zero, torch.zeros_like(expected_zero), atol=1e-5)
+
+    def _check_det_constraint(self) -> bool:
+        with torch.no_grad():
+            outer_det = self.a * self.d - self.c * self.b
+            inner_det = self.w * self.z - self.y * self.x
+            return qml.math.allclose(outer_det, inner_det, atol=1e-5)
+
+    def _check_is_matchgate(self):
+        if not self._check_m_m_dagger_constraint():
+            raise ValueError(r"The matchgate does not satisfy the M M^\dagger constraint.")
+        if not self._check_m_dagger_m_constraint():
+            raise ValueError(r"The matchgate does not satisfy the M^\dagger M constraint.")  # pragma: no cover
+        if not self._check_det_constraint():
+            raise ValueError(r"The matchgate does not satisfy the determinant constraint.")
--- src/matchcake/operations/rxx.py
+++ src/matchcake/operations/rxx.py
@@ -1,10 +1,12 @@
-from typing import Union
+from typing import Union, Optional
 
 import pennylane as qml
-from pennylane import numpy as pnp
+import torch
+from pennylane.typing import TensorLike
 
 from .. import matchgate_parameter_sets as mps
 from .matchgate_operation import MatchgateOperation
+from ..utils.torch_utils import to_tensor
 
 
 class Rxx(MatchgateOperation):
@@ -23,18 +25,11 @@ class Rxx(MatchgateOperation):
     where :math:`\theta` is a parameter, :math:`X_{j}` is the Pauli-X operator applied on the wire :math:`j`,
     and :math:`i` is the imaginary unit.
     """
-
     num_wires = 2
     num_params = 1
 
-    def __init__(self, params: Union[pnp.ndarray, list, tuple], wires=None, id=None, **kwargs):
-        shape = qml.math.shape(params)[-1:]
-        n_params = shape[0] if shape else 1
-        if n_params != self.num_params:
-            raise ValueError(f"{self.__class__.__name__} requires {self.num_params} parameters; got {n_params}.")
-        self._given_params = params
-        theta = params
-        m_params = mps.MatchgateStandardParams(
+    def __new__(cls, theta: TensorLike, wires=None, id=None, **kwargs):
+        return cls.from_std_params(
             a=qml.math.cos(theta / 2),
             b=-1j * qml.math.sin(theta / 2),
             w=qml.math.cos(theta / 2),
@@ -43,6 +38,44 @@ class Rxx(MatchgateOperation):
             z=qml.math.cos(theta / 2),
             c=-1j * qml.math.sin(theta / 2),
             d=qml.math.cos(theta / 2),
+            wires=wires,
+            id=id,
+            **kwargs
         )
-        kwargs["in_param_type"] = mps.MatchgateStandardParams
-        super().__init__(m_params, wires=wires, id=id, **kwargs)
+
+    # def __init__(
+    #         self,
+    #         params: TensorLike,
+    #         wires=None,
+    #         id=None,
+    #         default_dtype: torch.dtype = torch.complex128,
+    #         default_device: Optional[torch.device] = None,
+    #         **kwargs,
+    # ):
+    #     shape = qml.math.shape(params)[-1:]
+    #     n_params = shape[0] if shape else 1
+    #     if n_params != self.num_params:
+    #         raise ValueError(f"{self.__class__.__name__} requires {self.num_params} parameters; got {n_params}.")
+    #     self._given_params = params
+    #     if qml.math.get_interface(params) != "torch":
+    #         theta = to_tensor(params, dtype=default_dtype, device=default_device)
+    #     else:
+    #         theta = params
+    #     m_params = mps.MatchgateStandardParams(
+    #         a=qml.math.cos(theta / 2),
+    #         b=-1j * qml.math.sin(theta / 2),
+    #         w=qml.math.cos(theta / 2),
+    #         x=-1j * qml.math.sin(theta / 2),
+    #         y=-1j * qml.math.sin(theta / 2),
+    #         z=qml.math.cos(theta / 2),
+    #         c=-1j * qml.math.sin(theta / 2),
+    #         d=qml.math.cos(theta / 2),
+    #     )
+    #     # kwargs["in_param_type"] = mps.MatchgateStandardParams
+    #     batch_size = qml.math.shape(params)[0]
+    #     matrix = torch.zeros(batch_size, 4, 4, dtype=theta.dtype, device=theta.device)
+    #     matrix[..., 0, 0] = qml.math.cos(theta / 2)
+    #     matrix[..., 0, 3] = -1j * qml.math.sin(theta / 2)
+    #     matrix[..., 3, 0] = -1j * qml.math.sin(theta / 2)
+    #     matrix[..., 3, 3] = qml.math.cos(theta / 2)
+    #     super().__init__(matrix, wires=wires, id=id, **kwargs)
